37264072df5b92e483e29008f11ef325e354ecc3
matteo_daverio <matteo.daverio@mail.polimi.it>
2014-04-30 18:27:45 +0200

    First Commit

---
+++ matteo.daverio-valerio.demaria/src/main/java/it/polimi/deib/provaFinale2014/matteo/daverio_valerio/demaria/App.java
@@ -0,0 +1,13 @@
+package it.polimi.deib.provaFinale2014.matteo.daverio_valerio.demaria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
