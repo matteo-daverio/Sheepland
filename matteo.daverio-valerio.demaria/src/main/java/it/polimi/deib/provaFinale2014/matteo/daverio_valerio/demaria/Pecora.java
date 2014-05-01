015cd5ff558c8ce571e4da80b2f5ab391d9bd1ef
matteo_daverio <matteo.daverio@mail.polimi.it>
2014-05-01 15:48:01 +0200

    Aggiunta classe Pecora e PecoraNera

---
+++ matteo.daverio-valerio.demaria/src/main/java/it/polimi/deib/provaFinale2014/matteo/daverio_valerio/demaria/Pecora.java
@@ -0,0 +1,31 @@
+package it.polimi.deib.provaFinale2014.matteo.daverio_valerio.demaria;
+
+public class Pecora {
+
+	private int tipoPecora;
+	private int posizione;	
+	
+	// costruttori
+	
+	public Pecora(int posizione, int tipoPecora)  //  costruttore per pecora se si conosce posizione e il tipo di pecora desiderato
+	{
+		this.tipoPecora=tipoPecora;
+		this.posizione=posizione;
+	}
+	
+	public Pecora(int posizione)  //  costruttore per pecora che sceglie a random se sarà pecora, montone o agnello
+	{
+		this(posizione,(int)Math.random()*3);
+	}
+	
+	public Pecora()  //  costruttore vuoto che crea la pecora a Sheepburg
+	{
+		this(0,1);
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ matteo.daverio-valerio.demaria/src/main/java/it/polimi/deib/provaFinale2014/matteo/daverio_valerio/demaria/PecoraNera.java
@@ -0,0 +1,7 @@
+package it.polimi.deib.provaFinale2014.matteo.daverio_valerio.demaria;
+
+public class PecoraNera extends Pecora{
+	
+	
+	
+}
