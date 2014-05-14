ea2f66004ca5a8befededcc890f2581fbb9fbec0
ValerioDeMaria <valerio.demaria@mail.polimi.it>
2014-05-14 10:38:50 +0200

    Revert "Rimossa Classe App"
    This reverts commit be688013f28fb7cd77feb35526173274581b508c.

---
+++ matteo.daverio-valerio.demaria/src/main/java/it/polimi/deib/provaFinale2014/matteo/daverio_valerio/demaria/App.java
@@ -0,0 +1,14 @@
+package it.polimi.deib.provaFinale2014.matteo.daverio_valerio.demaria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Modifica di Valerio" );
+    }
+}
