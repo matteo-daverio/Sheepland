addce1b6340b34293130d479a10fd73270a73de8
ValerioDeMaria <valerio.demaria@mail.polimi.it>
2014-05-16 10:46:33 +0200

    pecora nera come singleton

--- matteo.daverio-valerio.demaria/src/main/java/it/polimi/deib/provaFinale2014/matteo/daverio_valerio/demaria/meccanicaDiGioco/PecoraNera.java
+++ matteo.daverio-valerio.demaria/src/main/java/it/polimi/deib/provaFinale2014/matteo/daverio_valerio/demaria/meccanicaDiGioco/PecoraNera.java
@@ -3,9 +3,20 @@ package it.polimi.deib.provaFinale2014.matteo.daverio_valerio.demaria.meccanicaD
 import it.polimi.deib.provaFinale2014.matteo.daverio_valerio.demaria.Costanti;
 
 public class PecoraNera extends Pecora {
+	private static PecoraNera instance;
 
-	public PecoraNera() {
+	// costruttore privato
+	private PecoraNera() 
+	{
 		super(Costanti.POSIZIONE_SHEEPBURG, Costanti.TIPO_PECORA_PECORANERA);
+	}
+
+	public static PecoraNera instance() //istanzia la pecora nera solo se non già esistente 
+	{
+		
+		if (instance == null)
+			instance = new PecoraNera();
+		return instance;
 	}
 
 }
